07466851ad8023e55896a743f98c03f6bb59a7f1
Alvin Lin <alvin.lin@stuypulse.com>
2015-08-07 17:55:13 -0400

    better output data

--- Bot.py
+++ Bot.py
@@ -1,13 +1,13 @@
-import pyautogui as auto
-import sys
-import time
-
 """
 NoKeyboardException is raised when we cannot find the On-Screen Keyboard.
 
 Bot is a class that can do programmable actions. Initialize a bot and set
 a series of actions for it to do.
 """
+
+import pyautogui as auto
+import sys
+import time
 
 auto.FAILSAFE = True
 
@@ -25,7 +25,19 @@ class Bot():
     else:
       raise NoKeyboardException("No On-Screen Keyboard found. "
                                 "Try redo-ing your screenshot")
-    self.hp_pots_used = 0;
+    self.time_created = time.time()
+    self.hp_pots_used = 0
+    self.mana_pots_used = 0
+
+  def __str__(self):
+    for i in range(0, 50):
+      print "\n"
+    print "Time started: %s" % time.ctime(self.time_created)
+    print "Time now: %s" % time.ctime()
+    print "Time elapsed: %d" % (time.time() - self.time_created)
+    print "=" * 80
+    print "Health potions used: %d" % self.hp_pots_used
+    print "Mana potions used: %d" % self.mana_pots_used
 
   def _moveTo(self, coord):
     auto.moveTo(self.osk[0] + coord[0], self.osk[1] + coord[1])
@@ -39,6 +51,10 @@ class Bot():
 
   def checkHealth(self, pot_key):
     if not auto.locateOnScreen('data/hp.png'):
-      self.hp_pots_used += 1
-      print "Health potions used: " + str(self.hp_pots_used)
       self.click(pot_key, 0.25)
+      self.hp_pots_used += 1
+
+  def checkMana(self, pot_key):
+    if not auto.locateOnScreen('data/mana.png'):
+      self.click(pot_key, 0.25)
+      self.mana_pots_used += 1
